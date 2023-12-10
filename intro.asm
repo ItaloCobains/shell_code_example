a4e990cd169bd6ea80c4e5619c753e330909e240
ItaloCobains <italobrandao.contato@gmail.com>
2023-12-09 23:58:06 -0300

    Fix stack values and update shell code

--- intro.asm
+++ intro.asm
@@ -7,16 +7,18 @@ include \masm32\include\msvcrt.inc
 includelib \masm32\lib\msvcrt.lib
 extern printf:proc
 
-.data
-    fmt db "%s", 0Ah ;formato
-    nome db "ÏtaloCobains" ;meu nome
 
 .code
     start:
         push ebp
         mov ebp, esp
-        push offset nome
-        push offset fmt
+        ; Push to stack an values   
+        push 00434241h ; 0X41 0X42 0X43
+        mov ebx, esp
+        push 00007325h; 0x25 0x73
+        mov edx, esp
+        push ebx
+        push edx
         call printf
         leave
         ret
--- shell_code_base.md
+++ shell_code_base.md
@@ -1,1 +1,1 @@
-/x55/x8B/xEC/x68/x03/x30/x40/x00/x68/x00/x30/x40/x00/xE8/x02/x00/x00/x00/xC9/xC3
+/x55/x8B/xEC/x68/x41/x42/x43/x00/x8B/xDC/x68/x25/x73/x00/x00/x8B/xD4/x53/x52/xE8/x02/x00/x00/x00/xC9/xC3
